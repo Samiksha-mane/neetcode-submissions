df1c15c4338f966245d3da808b1774b3ea086188
Samiksha-mane <samikshamane2005@gmail.com>
2026-06-26 08:42:47 +0530

    Bulk sync: 5 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-1.java
@@ -0,0 +1,21 @@
+class Solution 
+{
+    public boolean isAnagram(String s, String t) 
+    {
+        if(s.length() != t.length())
+            return false;
+        
+        int[] frq = new int[26];
+        for(int i = 0; i < s.length();i++)
+        {
+            frq[s.charAt(i) - 'a']++;
+            frq[t.charAt(i)- 'a']--;
+        }
+        for(int count : frq)
+        {
+            if(count != 0)
+                return false;
+        }
+        return true;
+    }
+}
